1d01145e1eb24f377eeb332076d37813ce416c5f
sonal <sonalkarle01@gmail.com>
2021-03-10 19:43:17 +0530

    Adding type of person in both

--- AddressBookSql/AddressBookService.sql
+++ AddressBookSql/AddressBookService.sql
@@ -62,3 +62,13 @@ SELECT * FROM AddressBookservice;
 --UC10:Ability to get  number of contact person
 SELECT Type, COUNT(*) AS TYPE_COUNT FROM AddressBookservice GROUP BY Type;
 
+
+--UC11:Ability to add in both family and friend
+INSERT INTO  AddressBookservice (FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email , Type)VALUES
+('Sachin', 'Shinde', 'Vikroli', 'Mumbai', 'Maharashtra', '40007', '8806184087', 'shinde@gmail.com', 'FRIENDS');
+
+
+
+
+
+
