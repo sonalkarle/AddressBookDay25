ea42b89f8f9bc0273380a4ff62d572fb51c4dd8c
sonal <sonalkarle01@gmail.com>
2021-03-10 18:54:55 +0530

    Welcome

--- AddressBookSql/SQLQuery1.sql
+++
@@ -1,6 +0,0 @@
---UC7:Ability to understand the size
-SELECT CITY, COUNT(CITY) AS CITY_COUNT FROM AddressBookservice GROUP BY City;
-SELECT STATE, COUNT(STATE) AS STATE_COUNT FROM AddressBookservice GROUP BY State;
-
-
-
