26d9f05c0fb46a79bd3e23b8eb1f2ac9efc164f2
sonal <sonalkarle01@gmail.com>
2021-03-10 06:51:13 +0530

    Ability to find size using city or state

--- AddressBookSql/SQLQuery1.sql
+++ AddressBookSql/SQLQuery1.sql
@@ -1,4 +1,6 @@
---UC6:Retrive the data from city and state
-SELECT * FROM AddressBookservice WHERE City = 'Mumbai';
-SELECT * FROM AddressBookservice WHERE State = 'Maharashtra';
+--UC7:Ability to understand the size
+SELECT CITY, COUNT(CITY) AS CITY_COUNT FROM AddressBookservice GROUP BY City;
+SELECT STATE, COUNT(STATE) AS STATE_COUNT FROM AddressBookservice GROUP BY State;
+
+
 
